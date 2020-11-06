67aa9f467aa8176645a8901e1151f351355fc2c8
alsals126 <9970mia@naver.com>
2020-11-07 00:21:14 +0900

    Complete Enter-code

---
+++ SmileGame/Assets/code/Enter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Enter : MonoBehaviour
+{
+    [SerializeField] private InputField input_plz;
+    [SerializeField] private string sceneName;
+    [SerializeField] private string text1;
+
+    public void enterButton()
+    {
+        if(input_plz.text == text1)
+        {
+            MoveScene();
+        }
+    }
+    public void MoveScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
