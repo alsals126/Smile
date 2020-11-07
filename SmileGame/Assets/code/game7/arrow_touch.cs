779e0a333bf04310297846eef513867d03f292b8
alsals126 <9970mia@naver.com>
2020-11-08 03:12:06 +0900

    우리 할 수 있어,,,

---
+++ SmileGame/Assets/code/game7/arrow_touch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class arrow_touch : MonoBehaviour
+{
+    private GameObject obj;
+    Vector3 pos;
+    private float x, y;
+    private void Start()
+    {
+        pos = this.gameObject.transform.position;
+        setX(pos.x);
+        setY(pos.y);
+    }
+    private void setX(float posX)  { x = posX; }
+    private void setY(float posY) { x = posY; }
+    public void OnMouseDown()
+    {
+        obj.transform.position = new Vector3(0, 0, 0);
+    }
+
+}
