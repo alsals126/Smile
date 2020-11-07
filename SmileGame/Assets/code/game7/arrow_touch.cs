cbe8ae70058674bd891c7b5e9d928e3311c4f427
alsals126 <9970mia@naver.com>
2020-11-08 04:09:23 +0900

    잘자

--- SmileGame/Assets/code/game7/arrow_touch.cs
+++ SmileGame/Assets/code/game7/arrow_touch.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class arrow_touch : MonoBehaviour
+// https://m.blog.naver.com/PostView.nhn?blogId=inceleb&logNo=220685447126&proxyReferer=https:%2F%2Fwww.google.com%2F
+public class arrow_touch : MonoBehaviour, IPointerDownHandler
 {
-    private GameObject obj;
+    [SerializeField] private GameObject obj;
     Vector3 pos;
     private float x, y;
     private void Start()
+    {
+        obj.transform.position = new Vector3(-2, 0.7f, 0); //처음 위치 설정
+        setPosition();
+    }
+    private void Update()
     {
         pos = this.gameObject.transform.position;
         setX(pos.x);
         setY(pos.y);
     }
-    private void setX(float posX)  { x = posX; }
-    private void setY(float posY) { x = posY; }
-    public void OnMouseDown()
+    private void setPosition()
     {
-        obj.transform.position = new Vector3(0, 0, 0);
+        
     }
+    private void setX(float posX)  { x = posX; }
+    private void setY(float posY) { y = posY; }
+    private float getX() { return x; }
+    private float getY() { return y; }
 
+    public void right(){ setPosition();  obj.transform.position = new Vector3(getX() + 0.01f, getY(), 0); }
+    public void left() { setPosition(); obj.transform.position = new Vector3(getX() - 1, getY(), 0); }
+    public void down() { setPosition(); obj.transform.position = new Vector3(getX(), getY() - 1, 0); }
+    public void up() { setPosition(); obj.transform.position = new Vector3(getX(), getY() + 1, 0); }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        setPosition(); obj.transform.position = new Vector3(getX() + 0.01f, getY(), 0);
+    }
 }
